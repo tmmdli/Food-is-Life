191118ad9f06e274b5534cd08ed76d5f535a9cc2
tmmdli <teranemmmdli23@.com>
2023-05-19 19:20:25 -0700

    link page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Favorites from './Favorites';
+import Categories from './Categories';
+
+const App = () => {
+  return (
+    <SafeAreaView  style={{flex: 1}}>
+      <Favorites />
+      {/* <Categories/> */}
+    
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
