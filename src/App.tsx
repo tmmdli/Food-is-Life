662b326a33bf2bee1be186f9a874dad6ec31fc97
tmmdli <teranemmmdli23@.com>
2023-05-25 13:59:46 -0700

    Launch_screen i added

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import Favorites from './Favorites';
 import Categories from './Categories';
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const App = () => {
   return (
+    <Provider store={store}  >
     <SafeAreaView  style={{flex: 1}}>
       <Favorites />
       {/* <Categories/> */}
     
     
     </SafeAreaView>
+    </Provider>
   );
 };
 
