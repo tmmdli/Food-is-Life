aeb648b0641551f78d8977f31f29c5b555e8bbfa
19Famil <129958208+19Famil@users.noreply.github.com>
2023-05-31 18:15:49 +0400

    add

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from '../screens/favorites/redux/slice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+  
   },
 });
 
